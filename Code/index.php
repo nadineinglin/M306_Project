f8fd05c28f82e95c0a51a4b1a5fa1720dd5dc1e9
nadineinglin <nadine.inglin@gmail.com>
2021-04-21 15:28:06 +0200

    Hallo

---
+++ Code/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <link rel="stylesheet" href="https://use.typekit.net/tri3onz.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <br>
+        <br>
+        <br>
+        <br>
+        <div class="row">
+            <div class="col-sm-2">
+                <h2>Regeln:</h2>
+                <p>Sobald der Spieler 3x hintereinander verliert ist das Spiel vorbei</p>
+            </div>
+            <div class="col-sm-2"></div>
+            <div class="jumbotron col-sm-4">
+                <div class="">
+                <h1>Login</h1>
+                <form action="?">
+                    <input type="text">
+                    <br>
+                    <button type="submit" class="sub">Los geht's!</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <br>
+        <div class="row">
+            <div class="col-sm-4">
+                <input id="paper" type="image" src="/icon/papier.png" alt="paper">
+            </div>
+            <div class="col-sm-4">
+                <input id="scissor" type="image" src="/icon/schere.png" alt="scissor">   
+            </div>
+            <div class="col-sm-4">
+                <input  id="rock"type="image" src="/icon/stein.png" alt="rock">    
+            </div>
+        </div>
+    </div>    
+        
+        
+</body>
+</html>
